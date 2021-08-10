1a7c8313ec2ab077f8bcd356b5d68bba99494cf2
Zach Holloway <zachholloway.n@gmail.com>
2021-08-09 22:51:16 -0400

    commiting to push to work remote

--- index.js
+++ index.js
@@ -2,8 +2,20 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
+
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    // 1: Project Title
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter project title',
+        validate: titleInput => {
+
+          
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
